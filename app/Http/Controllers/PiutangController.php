d67cf7621651ee204773ae684b6985c9a7ade33f
iamnadiaim <137363282+iamnadiaim@users.noreply.github.com>
2024-11-11 21:01:52 +0700

    Update PiutangController.php

--- app/Http/Controllers/PiutangController.php
+++ app/Http/Controllers/PiutangController.php
@@ -38,42 +38,5 @@ class PiutangController extends Controller
 
         return redirect()->route('piutang.index')
                          ->with('success', 'piutang berhasil ditambahkan');
-    }
-
-    public function show($id)
-    {
-        $piutang = piutang::findOrFail($id);
-        if (!$piutang) {
-            return abort(404); // Handle record not found
-        }
-        return view('piutang.show', compact('piutang'));
-    }
-
-    public function edit($id)
-    {
-        $piutang = piutang::findOrFail($id);
-        if (!$piutang) {
-            return abort(404); // Handle record not found
-        }
-        return view('piutang.edit', compact('piutang'));
-    }
-
-     public function update(Request $request, $id)
-     {
-         $request->validate([
-             'jumlah_piutang' => 'required|numeric|min:1',
-             'sisa_piutang' => 'required|numeric|min:0',
-         ]); // Remove validation for nama, jumlah_piutang, and tanggal
-
-         $piutang = piutang::find($id); // Use find instead of findOrFail
-
-         if (!$piutang) {
-             return abort(404); // Handle record not found
-         }
-
-         $piutang->update($request->all());
-
-         return redirect()->route('piutang.index')
-             ->with('success', 'piutang berhasil diperbarui');
-     }
+    }    
 }
