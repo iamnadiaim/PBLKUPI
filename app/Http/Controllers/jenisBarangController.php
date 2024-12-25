48aafade556985fe7f5fe6ab6c6bedd721abc0bf
iamnadiaim <137363282+iamnadiaim@users.noreply.github.com>
2024-12-25 22:29:40 +0700

    Update jenisBarangController.php

--- app/Http/Controllers/jenisBarangController.php
+++ app/Http/Controllers/jenisBarangController.php
@@ -10,35 +10,34 @@ class jenisBarangController extends Controller
 {
     public function index()
     {
+        // return view('beban.kategori');
         $produk = Produk::all();
         $jenis_barangs = jenisBarang::all();
 
         return view('produk.jenisBarang', compact('produk', 'jenis_barangs'));
     }
-
     public function store(Request $request)
     {
-        $validate = $request->validate([
+    $validate = $request->validate([
         "nama" => [
-            "required|string",
-            function ($attribute, $value, $fail) {
-                // Periksa apakah data sudah ada
-                if (jenisBarang::where('nama', $value)
-                    ->where('id_usaha', auth()->user()->id_usaha)
-                    ->exists()) {
-                    $fail("Jenis barang sudah ada.");
-                }
-            }
-        ],
+            "required",
+            "regex:/^[a-zA-Z\s]+$/", // Mengizinkan huruf dan spasi saja
+            "not_regex:/^\d+$/", // Tidak boleh hanya angka
+            "unique:jenis_barangs,nama" // Validasi unik untuk kolom nama
+        ]
     ], [
-        "nama.required" => "Nama jenis barang harus diisi.", // Jika kosong
-        "nama.string" => 'Nama jenis barang harus berupa huruf',
+        "nama.required" => "Jenis Produk harus diisi.",
+        "nama.regex" => "format tidak sesuai",
+        "nama.not_regex" => "Jenis Produk tidak sesuai format.",
+        "nama.unique" => "Data sudah ada." // Pesan error jika data sudah ada
     ]);
 
-        $validate['id_usaha'] = auth()->user()->id_usaha;
+    $validate['id_usaha'] = auth()->user()->id_usaha;
 
-        jenisBarang::create($validate);
-        return redirect()->route('produks.create')
-            ->with('tambah', 'Jenis Barang berhasil ditambahkan');
+    jenisBarang::create($validate);
+    return redirect()->route('produks.create')
+        ->with('tambah', 'Kategori berhasil ditambahkan');
     }
+
+
 }
