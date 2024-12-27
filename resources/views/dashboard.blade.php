c6994ef09ee68771ce3d2286bb0ee19dfb7a89e5
iamnadiaim <137363282+iamnadiaim@users.noreply.github.com>
2024-12-27 18:15:48 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -170,23 +170,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Grafik Donut untuk Produk Terlaris
     if (totalProdukTerlaris > 0) {
-    let seriesProduk = produkTerlaris.map(produk => produk.total_terjual); 
+    let seriesProduk = produkTerlaris.map(produk => produk.total_terjual > 1 ? produk.total_terjual : 1);
     let labelsProduk = produkTerlaris.map(produk => produk.nama_produk); 
 
     var produkOptions = {
-        series: seriesProduk,
-        chart: {
-            type: 'donut',
-        },
-        labels: labelsProduk,
-        responsive: [{
-            breakpoint: 480,
-            options: {
-                chart: { width: 200 },
-                legend: { position: 'bottom' }
+    series: seriesProduk,
+    chart: {
+        type: 'donut',
+    },
+    labels: labelsProduk,
+    plotOptions: {
+        pie: {
+            donut: {
+                size: '50%',
+                labels: {
+                    show: true
+                }
             }
-        }]
-    };
+        }
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: { width: 200 },
+            legend: { position: 'bottom' }
+        }
+    }]
+};
+
 
     var produkPieChart = new ApexCharts(document.querySelector("#produkPieChart"), produkOptions);
     produkPieChart.render();
